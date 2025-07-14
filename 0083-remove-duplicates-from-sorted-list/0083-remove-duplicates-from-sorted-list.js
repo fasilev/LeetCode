4e2cda025233328d685b87a06d1d737a03373133
fasilev <fazi56782084@gmail.com>
2025-07-14 11:25:43 +0530

    Time: 1 ms (39.43%), Space: 57.7 MB (54.68%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+  if (head === null) return null;
+    
+    let duplicates = head;
+    
+    while (head && head.next) {
+        if (head.val === head.next.val) {
+            head.next = head.next.next;
+        } else {
+            head = head.next;
+        }
+    }
+    
+    return duplicates;
+    
+};
